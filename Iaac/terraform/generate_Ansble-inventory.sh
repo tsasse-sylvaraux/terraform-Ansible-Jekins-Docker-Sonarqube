41233393a4c75605f430cc3454483f28992a0cbd
tsasse-sylvaraux <sylvatsasse2021@gmail.com>
2025-05-08 18:28:20 +0200

    Iaac add terra

--- Iaac/terraform/generate_Ansble-inventory.sh
+++ Iaac/terraform/generate_Ansble-inventory.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "[jenkins]" > ../ansible/inventory.ini
+echo "[jenkins]" > ../Ansible/inventory.ini
 terraform output -json | jq -r '.public_ips.value[0]' | \
-  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../ansible/inventory.ini
+  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../Ansible/inventory.ini
 
-echo "[sonarqube]" >> ../ansible/inventory.ini
+echo "[sonarqube]" >> ../Ansible/inventory.ini
 terraform output -json | jq -r '.public_ips.value[1]' | \
-  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../ansible/inventory.ini
+  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../Ansible/inventory.ini
 
-echo "[docker]" >> ../ansible/inventory.ini
+echo "[docker]" >> ../Ansible/inventory.ini
 terraform output -json | jq -r '.public_ips.value[2]' | \
-  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../ansible/inventory.ini
+  xargs -I{} echo "{} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../Ansible/inventory.ini
 
